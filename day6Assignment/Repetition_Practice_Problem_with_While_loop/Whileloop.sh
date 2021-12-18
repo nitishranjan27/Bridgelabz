10431457eba6f8fb052b716a7b0fa0c2c401076a
Nitish <nk47595@gmail.com>
2021-12-18 18:57:14 +0530

    Assignments

--- day6Assignment/Repetition_Practice_Problem_with_While_loop/Whileloop.sh
+++ day6Assignment/Repetition_Practice_Problem_with_While_loop/Whileloop.sh
@@ -1,10 +1,13 @@
 Result=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]]; 
-then
-    echo HEADS
-elif [[ ${Result} -eq 1 ]]; 
-then
-    echo TAILS
-else
-    echo "Coin Wrong"
-fi
+while [ ${Result} -lt 11 ]
+do
+ if [[ ${Result} -eq 0 ]]; 
+ then
+    echo "HEADS"
+ elif [[ ${Result} -eq 1 ]]; 
+ then
+     echo "TAILS"
+ else
+     echo "Coin Wrong"
+ fi
+done
